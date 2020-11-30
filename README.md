ff5a4488d7757e880ef88f2f8be6cbcf74dc879a
Karol P <karprzy7@gmail.com>
2020-11-30 12:59:40 +0100

    Minor refactoring of device_mod

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # drivers_fun
+
+
+
+
+
+
+
+## Sources:
+
+https://stackoverflow.com/questions/34522426/unable-to-write-on-dev-files # Writing do /dev as normal user
+
+https://linux-kernel-labs.github.io/refs/heads/master/labs/device_drivers.html # kernel cdriver tutorial
--- device_files/device_mod.c
+++ device_files/device_mod.c
@@ -5,82 +5,81 @@
 #include <linux/device.h>
 #include <linux/gpio.h>
 
-#define DEVICE_NAME "dummy"
-#define MAJOR_NUM 42
+#define DEVICE_NAME "modx"
+#define MAJOR_NUM 42  // as this is an answer to everything
 
-static struct class *dummy_class;
+static struct class *modx_class;
 
 // GPIO 
-static unsigned int gpioLED = 21;       ///< hard coding the LED gpio for this example to P9_23 (GPIO49)
-static bool	    ledOn = 0;          ///< Is the LED on or off? Used to invert its state (off by default)
+static unsigned int gpioLED = 21;       ///<hard coding the LED gpio for this example 
+static bool	    ledOn = 0;          ///< used to invert state
 
 
-static int dummy_open(struct inode *inode, struct file *file)
+static int modx_open(struct inode *inode, struct file *file)
 {
 	pr_info("%s\n", __func__);
 	return 0;
 }
 
-static int dummy_release(struct inode *inode, struct file *file)
+static int modx_release(struct inode *inode, struct file *file)
 {
 	pr_info("%s\n", __func__);
 	return 0;
 }
 
-static ssize_t dummy_read(struct file *file,
+static ssize_t modx_read(struct file *file,
 			char *buffer, size_t length, loff_t * offset)
 {
 	pr_info("%s %u\n", __func__, length);
 	return 0;
 }
 
-static ssize_t dummy_write(struct file *file,
+static ssize_t modx_write(struct file *file,
 			 const char *buffer, size_t length, loff_t * offset)
 {
 	pr_info("%s %u\n", __func__, length);
 	return length;
 }
 
-struct file_operations dummy_fops = {
+struct file_operations modx_fops = {
 	.owner = THIS_MODULE,
-	.open = dummy_open,
-	.release = dummy_release,
-	.read = dummy_read,
-	.write = dummy_write,
+	.open = modx_open,
+	.release = modx_release,
+	.read = modx_read,
+	.write = modx_write,
 };
 
-int __init dummy_init(void)
+int __init modx_init(void)
 {
 	int ret;
 
 	printk("Module X loaded\n");
-	ret = register_chrdev(MAJOR_NUM, DEVICE_NAME, &dummy_fops);
+	ret = register_chrdev(MAJOR_NUM, DEVICE_NAME, &modx_fops);
 	if (ret != 0)
 		return ret;
 
-	dummy_class = class_create(THIS_MODULE, DEVICE_NAME);
+	modx_class = class_create(THIS_MODULE, DEVICE_NAME);
 
-	device_create(dummy_class, NULL, MKDEV(MAJOR_NUM, 0), NULL, "dummy%d", 0);
+	device_create(modx_class, NULL, MKDEV(MAJOR_NUM, 0), NULL, "modx%d", 0);
 
     // Is the GPIO a valid GPIO number
     if (!gpio_is_valid(gpioLED)){
         printk(KERN_INFO "GPIO_TEST: invalid LED GPIO\n");
-        return -ENODEV;
+        return -ENODEV;   
     }
-    // Going to set up the LED. It is a GPIO in output mode and will be on by default
+
     ledOn = true;
-    gpio_request(gpioLED, "sysfs");          // gpioLED is hardcoded to 49, request it
-    gpio_direction_output(gpioLED, ledOn);   // Set the gpio to be in output mode and on
-    // gpio_set_value(gpioLED, ledOn);          // Not required as set by line above (here for reference)
-    gpio_export(gpioLED, false);             // Causes gpio to appear in /sys/class/gpio
+    gpio_request(gpioLED, "sysfs");          
+    gpio_direction_output(gpioLED, ledOn);   
+    gpio_export(gpioLED, false);      
 
 	return 0;
 }
 
-void __exit dummy_exit(void)
+void __exit modx_exit(void)
 {
-	device_destroy(dummy_class, MKDEV(MAJOR_NUM, 0));
-	class_destroy(dummy_class);
+	device_destroy(modx_class, MKDEV(MAJOR_NUM, 0));
+	class_destroy(modx_class);
 
 	unregister_chrdev(MAJOR_NUM, DEVICE_NAME);
     gpio_set_value(gpioLED, 0);              // Turn the LED off
@@ -89,8 +88,8 @@ void __exit dummy_exit(void)
     printk(KERN_INFO "Module X going down\n");
 }
 
-module_init(dummy_init);
-module_exit(dummy_exit);
+module_init(modx_init);
+module_exit(modx_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Karol Przybylski");
 MODULE_DESCRIPTION("A simple GPIO driver");
